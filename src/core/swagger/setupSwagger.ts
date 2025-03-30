d4febf5b8a7aeff68bc131a8de51c67a527c3d7c
al_trapeznikov <al.trapeznikov.743@gmail.com>
2025-03-30 23:07:24 +0400

    added use of CDN version of Swagger UI

--- src/core/swagger/setupSwagger.ts
+++ src/core/swagger/setupSwagger.ts
@@ -17,29 +17,16 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
-  /* const swaggerUiOptions = {
-    customCss: `
-      body {
-        background-color: #1e1e1e;
-        color: white;
-      }
-      .swagger-ui .topbar {background-color: #333;}
-      .swagger-ui .info {color: white;}
-      .swagger-ui .response-col_status_200 {background-color: #2e7d32;}
-      .swagger-ui .response-col_status_400 {background-color: #d32f2f;}
-      .swagger-ui .response-col_status_500 {background-color: #f44336;}
-      .swagger-ui .scheme-container .auth-wrapper .auth {
-        color: white;
-      }
-      .swagger-ui .opblock-summary-path {
-        color: #ffffff;
-      }
-      .swagger-ui .opblock-summary-description {
-        color: #e0e0e0;
-      }
-    `,
-    customSiteTitle: 'My API Documentation',
-  }; */
 
-  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec, /* swaggerUiOptions */));
+
+  app.use(
+    '/api',
+    swaggerUi.serve,
+    swaggerUi.setup(
+      swaggerSpec,
+      {
+        customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      }
+    ));
 };
